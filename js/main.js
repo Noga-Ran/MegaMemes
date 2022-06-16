0ec7cd685aceae1dcb8b0ba9251de5a0b3c33d91
Noga <nogaran01@gmail.com>
2022-06-16 15:57:47 +0300

    css layout

--- js/main.js
+++ js/main.js
@@ -3,18 +3,3 @@
 function onInit() {
     renderGallery()
 }
-
-function displayGallery(){
-    var elGallery = document.getElementById('gallery')
-    elGallery.style.display = ''
-    var elEditor = document.querySelector('.editor')
-    elEditor.style.display = 'none'
-}
-
-function hideGallery(){
-    var elGallery = document.getElementById('gallery')
-    elGallery.style.display = 'none'
-    var elEditor = document.querySelector('.editor')
-    
-    elEditor.style.display = 'block'
-}
