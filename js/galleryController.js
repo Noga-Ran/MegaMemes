56724d4a1cdfab1659479f185edbf39e8675cfa1
Noga <nogaran01@gmail.com>
2022-06-17 15:27:14 +0300

    layout

--- js/galleryController.js
+++ js/galleryController.js
@@ -6,7 +6,7 @@ function renderGallery() {
     var newImagsValues= []
 
     for(var i=1; i<=18 ; i++){
-        newHtml+=`<div class="div-${i}"><img id="${i}" src="meme-imgs/${i}.jpg" width="250" height="250" onclick="setImg(${i}); hideGallery();"></div>`
+        newHtml+=`<div class="div-${i}"><img class="gallery-imgs" id="${i}" src="meme-imgs/${i}.jpg" width="250" height="250" onclick="setImg(${i}); hideGallery();"></div>`
         newImagsValues.push({id:`${i}`,url:`meme-imgs/${i}.jpg`})
     }
     elGalleryC.innerHTML = newHtml
@@ -19,12 +19,15 @@ function displayGallery(){
     elGallery.style.display = ''
     var elEditor = document.querySelector('.editor')
     elEditor.style.display = 'none'
+    var elAbout=document.getElementById('about')
+    elAbout.style.display = ''
 }
 
 function hideGallery(){
     var elGallery = document.getElementById('gallery')
     elGallery.style.display = 'none'
     var elEditor = document.querySelector('.editor')
-    
     elEditor.style.display = 'block'
+    var elAbout=document.getElementById('about')
+    elAbout.style.display = 'none'
 }
